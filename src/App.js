402e47972a4f81589d709db1f02df6ca95a0f2f9
ParkSaseong <78298561+ParkSaseong@users.noreply.github.com>
2024-07-17 14:11:46 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import {useState, useEffect} from "react";
+import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
+import './App.css';
+import Movie from "./Movie";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/movie/:id" element={<Detail />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
